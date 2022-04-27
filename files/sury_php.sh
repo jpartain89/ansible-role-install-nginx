fab6ff96216a3dd80eaa4c157ea83ba97dd9297a
Justin Partain <jpartain89@gmail.com>
2022-04-26 23:25:00 -0500

    Updates

--- files/sury_php.sh
+++ files/sury_php.sh
@@ -1,4 +1,16 @@
 #!/usr/local/env bash
 set -e
 
-wget -O - https://packages.sury.org/php/README.txt | sudo bash -
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+if [[ $(lsb_release -sc) == "focal" ]]; then
+    OS_VERS=bullseye
+fi
+
+${SUDO} apt-get update
+${SUDO} apt-get -y install apt-transport-https lsb-release ca-certificates curl
+${SUDO} curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ ${OS_VERS} main" | ${SUDO} tee /etc/apt/sources.list.d/php.list
+${SUDO} apt-get update
